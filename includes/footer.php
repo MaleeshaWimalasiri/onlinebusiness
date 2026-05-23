5551dd8ebd33ec77edd950b7ea92f6d478f57261
MaleeshaWimalasiri <pmnmwimalasiri@students.nsbm.ac.lk>
2026-05-23 23:23:53 +0530

    add a footer

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+    </main>
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row gy-4">
+                <div class="col-md-4">
+                    <h5 class="footer-brand"><span class="brand-mark">&#10070;</span> <?= e(SITE_NAME) ?></h5>
+                    <p class="small">Handcrafted fine jewelry for life's most precious moments. Ethically sourced, expertly finished.</p>
+                </div>
+                <div class="col-md-4">
+                    <h6>Quick Links</h6>
+                    <ul class="list-unstyled small">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="products.php">Shop All</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h6>Visit Us</h6>
+                    <p class="small mb-1">No. 45, Galle Road, Colombo 03, Sri Lanka</p>
+                    <p class="small mb-1">Phone: +94 11 234 5678</p>
+                    <p class="small">Email: hello@maheeshajewels.com</p>
+                </div>
+            </div>
+            <hr class="footer-divider">
+            <p class="text-center small mb-0">&copy; <?= date('Y') ?> <?= e(SITE_NAME) ?>. All rights reserved.</p>
+        </div>
+    </footer>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
